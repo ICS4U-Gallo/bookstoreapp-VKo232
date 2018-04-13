93c3bc9c67b6441f6d17f56d16a75f14580c8130
qwertyuiop232 <vk07275@gmail.com>
2018-04-13 14:14:52 -0400

    video 12 done

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -3,6 +3,8 @@ package controllers;
 import models.Book;
 import play.mvc.Controller;
 import play.mvc.Result;
+import java.util.Set;
+import views.html.books.*;
 
 public class BooksController extends Controller {
 
@@ -10,7 +12,7 @@ public class BooksController extends Controller {
     // for all books
     public Result index() {
         Set<Book> books = Book.allBooks();
-        return ;
+        return ok(index.render(books));
     }
 
     public Result create() {
