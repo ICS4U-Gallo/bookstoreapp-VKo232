69f5b58d592d662872a8ad222bd0647a70bcbb50
qwertyuiop232 <vk07275@gmail.com>
2018-04-13 18:10:47 -0400

    video 19 done

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -61,7 +61,14 @@ public class BooksController extends Controller {
     }
 
     public Result destroy(Integer id) {
-        return TODO;
+        Book book = Book.findById(id);
+        if(book == null) {
+            return notFound("Book not found");
+        }
+
+        Book.remove(book);
+
+        return redirect(routes.BooksController.index());
     }
 
     public Result show(Integer id) {
