56ddb64c0eb7886423afa18c228a6c3c03e35d6f
qwertyuiop232 <vk07275@gmail.com>
2018-04-18 12:38:02 -0400

    video 19 done

--- app/models/Book.java
+++ app/models/Book.java
@@ -47,4 +47,37 @@ public class Book {
 
         return books.remove(book);
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPrice() {
+        return price;
+
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
